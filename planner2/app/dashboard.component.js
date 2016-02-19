14e1198a82cc7921ee16bfc4cc040afaa2c85ea1
jrisk <joeyrsk@gmail.com>
2016-02-19 16:51:31 -0600

    dashboard url method in component

--- planner2/app/dashboard.component.js
+++ planner2/app/dashboard.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', './plan.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,23 +8,36 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, plan_service_1;
     var DashboardComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (plan_service_1_1) {
+                plan_service_1 = plan_service_1_1;
             }],
         execute: function() {
             DashboardComponent = (function () {
-                function DashboardComponent() {
+                function DashboardComponent(_planService) {
+                    this._planService = _planService;
+                    this.plans = [];
                 }
+                DashboardComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    this._planService.getPlans()
+                        .then(function (plans) { return _this.plans = plans.slice(2, 3); });
+                };
+                DashboardComponent.prototype.goToDetail = function () {
+                    /* not implemented */
+                };
                 DashboardComponent = __decorate([
                     core_1.Component({
                         selector: 'dashboard',
-                        template: "<h2>My DashBoard</h2>",
+                        templateUrl: '../public/dashboard.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [plan_service_1.PlanService])
                 ], DashboardComponent);
                 return DashboardComponent;
             })();
--- planner2/app/dashboard.component.ts
+++ planner2/app/dashboard.component.ts
@@ -1,11 +1,25 @@
-import {Component} from 'angular2/core'
+import {Component, OnInit} from 'angular2/core'
+import {Plan} from './plan'
+import {PlanService} from './plan.service'
 
 @Component({
 	selector: 'dashboard',
-	template: `<h2>My DashBoard</h2>`,
-
+	templateUrl: '../public/dashboard.component.html'
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+	public plans: Plan[] = [];
+
+	constructor(private _planService: PlanService) { }
+
+	ngOnInit() {
+		this._planService.getPlans()
+			.then(plans => this.plans = plans.slice(2, 3));
+	}
+
+	goToDetail() {
+		/* not implemented */
+	}
 
 }
